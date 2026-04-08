bfbdebc33ae7955c9f132501073539628034a379
shreeharims <sriharims1945@gmail.com>
2026-04-09 01:19:58 +0530

    Add simple HTTP server and Dockerfile

---
+++ app.py
@@ -0,0 +1,25 @@
+from http.server import *
+
+PORT = 5020
+
+class MyHandler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Hello from Docker")
+
+HTTPServer(("0.0.0.0", PORT), MyHandler).serve_forever()
+
+
+
+## Dockerfile for app.py (Web Server):
+
+FROM python:3.9-slim
+
+WORKDIR /app
+
+COPY app.py .
+
+EXPOSE 5020
+
+CMD ["python", "app.py"]
